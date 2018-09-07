7a3eed136a2a0e462f9b1341fabced4f2e15f51e
Paul Woolcock <paul@woolcock.us>
2018-09-06 23:04:08 -0400

    Change `search_accounts` to use a macro

--- src/lib.rs
+++ src/lib.rs
@@ -46,7 +46,6 @@ extern crate doc_comment;
 extern crate isolang;
 #[macro_use]
 extern crate serde_json;
-extern crate serde_urlencoded;
 extern crate chrono;
 extern crate reqwest;
 extern crate serde;
